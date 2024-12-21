a6615a88849c6aabfc7a4076ea9525c81af9138f
emna-dammak <emna.dammak@insat.ucar.tn>
2024-12-21 13:13:39 +0100

    feat: first version of the app

---
+++ src/graphql/queries.ts
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+
+export const SEARCH_USERS = gql`
+  query SearchUsers($query: String!) {
+    search(query: $query, type: USER, first: 10) {
+      edges {
+        node {
+          ... on User {
+            login
+            name
+            avatarUrl
+            bio
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_REPOSITORIES = gql`
+  query GetRepositories($username: String!) {
+    user(login: $username) {
+      repositories(first: 10) {
+        nodes {
+          id
+          name
+          description
+          primaryLanguage {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
