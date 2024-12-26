3d2b5c7a6d08efe5dc8610f73de64ed1c77b91be
emna-dammak <emna.dammak@insat.ucar.tn>
2024-12-26 01:57:42 +0100

    fix : removed unnecessary imports

--- src/components/NotFound.test.tsx
+++ src/components/NotFound.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import NotFound, { NotFoundProps } from "./NotFound";
-import React from "react";
 
 describe("NotFound Component", () => {
   const defaultProps: NotFoundProps = {
